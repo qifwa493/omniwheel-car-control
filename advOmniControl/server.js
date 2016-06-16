a2c7abcc63b4cf863aac8c62b38136e3253a19be
Edward <awful123456@hotmail.com>
2016-06-16 16:56:52 +0900

    Update server.js

--- advOmniControl/server.js
+++ advOmniControl/server.js
@@ -4,6 +4,8 @@ var fs = require("fs");
 var rpio = require("rpio");
 var port = 8080;
 var intervalId = [,,,];
+var speed = 10;
+var lastCommand;
 
 rpio.open(31, rpio.OUTPUT, 0);
 console.log("GPIO 31 opened");
@@ -96,100 +98,110 @@ io.sockets.on("connection", function (socket) {
 	
 	socket.on("button", function (data) {
 		console.log("Received: " + data);
+		socket.emit("message", data + " received");
 		
-		if(data == "emStop") {
-			socket.emit("message", "Emergency Stop received");
-			clearInterval(intervalId[0]);
-			clearInterval(intervalId[1]);
-			clearInterval(intervalId[2]);
-			clearInterval(intervalId[3]);
-			setPin(31, 1, 0);
-			setPin(32, 2, 0);
-			setPin(33, 1, 0);
-			setPin(35, 3, 0);
-			setPin(36, 2, 0);
-			setPin(37, 3, 0);
-			setPin(38, 4, 0);
-			setPin(40, 4, 0);
+		if(data != 'low' && data != 'mid' && data != 'high') {
+			handleCommand(data);
 		}
-		if(data == "front") {
-			socket.emit("message", "front received");
-			setMotor(1, 'on');
-			setMotor(2, 'back');
-			setMotor(3, 'back');
-			setMotor(4, 'on');
+		
+		if(data == 'low') {
+			speed = 20;
+			handleCommand(lastCommand);
 		}
-		if(data == "stop") {
-			socket.emit("message", "stop received");
-			setMotor(1, 'off');
-			setMotor(2, 'off');
-			setMotor(3, 'off');
-			setMotor(4, 'off');
+		if(data == 'mid') {
+			speed = 10;
+			handleCommand(lastCommand);
 		}
-		if(data == "back") {
-			socket.emit("message", "back received");
-			setMotor(1, 'back');
-			setMotor(2, 'on');
-			setMotor(3, 'on');
-			setMotor(4, 'back');
-		}
-		if(data == "left") {
-			socket.emit("message", "left received");
-			setMotor(1, 'on');
-			setMotor(2, 'on');
-			setMotor(3, 'back');
-			setMotor(4, 'back');
-		}
-		if(data == "right") {
-			socket.emit("message", "right received");
-			setMotor(1, 'back');
-			setMotor(2, 'back');
-			setMotor(3, 'on');
-			setMotor(4, 'on');
-		}
-		if(data == "frLe") {
-			socket.emit("message", "frLe received");
-			setMotor(1, 'on');
-			setMotor(2, 'off');
-			setMotor(3, 'back');
-			setMotor(4, 'off');
-		}
-		if(data == "frRi") {
-			socket.emit("message", "frRi received");
-			setMotor(1, 'off');
-			setMotor(2, 'back');
-			setMotor(3, 'off');
-			setMotor(4, 'on');
-		}
-		if(data == "baLe") {
-			socket.emit("message", "baLe received");
-			setMotor(1, 'off');
-			setMotor(2, 'on');
-			setMotor(3, 'off');
-			setMotor(4, 'back');
-		}
-		if(data == "baRi") {
-			socket.emit("message", "baRi received");
-			setMotor(1, 'back');
-			setMotor(2, 'off');
-			setMotor(3, 'on');
-			setMotor(4, 'off');
-		}
-		if(data == "cw") {
-			socket.emit("message", "cw received");
-			setMotor(1, 'back');
-			setMotor(2, 'back');
-			setMotor(3, 'back');
-			setMotor(4, 'back');
-		}
-		if(data == "ccw") {
-			socket.emit("message", "ccw received");
-			setMotor(1, 'on');
-			setMotor(2, 'on');
-			setMotor(3, 'on');
-			setMotor(4, 'on');
+		if(data == 'high') {
+			speed = 5;
+			handleCommand(lastCommand);
 		}
 		
 		console.log('completed\n');
 	});
 });
+
+function handleCommand(command) {
+	lastCommand = command;
+	
+	if(command == "emStop") {
+		clearInterval(intervalId[0]);
+		clearInterval(intervalId[1]);
+		clearInterval(intervalId[2]);
+		clearInterval(intervalId[3]);
+		setPin(31, 1, 0);
+		setPin(32, 2, 0);
+		setPin(33, 1, 0);
+		setPin(35, 3, 0);
+		setPin(36, 2, 0);
+		setPin(37, 3, 0);
+		setPin(38, 4, 0);
+		setPin(40, 4, 0);
+	}
+	if(command == "front") {
+		setMotor(1, 'on');
+		setMotor(2, 'back');
+		setMotor(3, 'back');
+		setMotor(4, 'on');
+	}
+	if(command == "stop") {
+		setMotor(1, 'off');
+		setMotor(2, 'off');
+		setMotor(3, 'off');
+		setMotor(4, 'off');
+	}
+	if(command == "back") {
+		setMotor(1, 'back');
+		setMotor(2, 'on');
+		setMotor(3, 'on');
+		setMotor(4, 'back');
+	}
+	if(command == "left") {
+		setMotor(1, 'on');
+		setMotor(2, 'on');
+		setMotor(3, 'back');
+		setMotor(4, 'back');
+	}
+	if(command == "right") {
+		setMotor(1, 'back');
+		setMotor(2, 'back');
+		setMotor(3, 'on');
+		setMotor(4, 'on');
+	}
+	if(command == "frLe") {
+		setMotor(1, 'on');
+		setMotor(2, 'off');
+		setMotor(3, 'back');
+		setMotor(4, 'off');
+	}
+	if(command == "frRi") {
+		setMotor(1, 'off');
+		setMotor(2, 'back');
+		setMotor(3, 'off');
+		setMotor(4, 'on');
+	}
+	if(command == "baLe") {
+		setMotor(1, 'off');
+		setMotor(2, 'on');
+		setMotor(3, 'off');
+		setMotor(4, 'back');
+	}
+	if(command == "baRi") {
+		setMotor(1, 'back');
+		setMotor(2, 'off');
+		setMotor(3, 'on');
+		setMotor(4, 'off');
+	}
+	if(command == "cw") {
+		setMotor(1, 'back');
+		setMotor(2, 'back');
+		setMotor(3, 'back');
+		setMotor(4, 'back');
+	}
+	if(command == "ccw") {
+		setMotor(1, 'on');
+		setMotor(2, 'on');
+		setMotor(3, 'on');
+		setMotor(4, 'on');
+	}
+}
